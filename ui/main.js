da6edc3c295647f07bc9a893df8d963939b1501a
rafeeqpk36 <rafeeqpk36@gmail.com>
2017-04-16 16:26:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -141,7 +141,7 @@ function loadLoginForm () {
 	
 	// The first thing to do is to check if the user is logged in!
 	loadLogin();
-	loadRegister();
+	
 	// Now this is something that we could have directly done on the server-side using templating too!
 	loadArticles();
 
